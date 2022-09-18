e51cb8fd85985efe116c2d420fbc063473a56ea2
Mendoza Holger <mendozadavidh111@gmail.com>
2022-09-18 09:09:20 -0400

    added some features still not complete

--- main.js
+++ main.js
@@ -5,26 +5,58 @@ addEventListener("DOMContentLoaded", (e) => {
   const searchBar = document.getElementById("searchBar");
   const displayAnime = document.getElementById("display-anime");
 
-  fetch("https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=0")
+  fetch("https://api.jikan.moe/v4/anime")
     .then((animes) => animes.json())
     .then((animes) => {
       animes.data.forEach((anime) => {
-        let article = document.createElement("article");
-        let h3 = document.createElement("h3");
-        let img = document.createElement("img");
-        let dateStart = document.createElement('p')
-        let dateEnd = document.createElement('p')
-        let statusP = document.createElement('p')
-        img.setAttribute("src", anime.attributes.posterImage.small);
-        img.setAttribute("alt", `photo of ${anime.attributes.canonicalTitle}`)
-        displayAnime.append(article);
-        h3.textContent = anime.attributes.canonicalTitle;
-        dateStart.textContent= anime.attributes.startDate[0]
-        article.append(h3);
-        article.prepend(img);
-        article.append(dateStart)
-        console.log(anime);
+        animeCard(anime);
+        // console.log(anime);
       });
     });
+
+  const animeCard = (anime) => {
+    let article = document.createElement("article");
+    let h3 = document.createElement("h3");
+    let img = document.createElement("img");
+    let statusP = document.createElement("p");
+    let p = document.createElement("p");
+    let animeRun = document.createElement("p");
+
+    img.setAttribute("src", anime.images.jpg.image_url);
+    img.setAttribute("alt", `photo of ${anime.title}`);
+    h3.textContent = anime.title;
+    p.textContent = displayAnime.append(article);
+    animeRun.textContent = anime.aired.string;
+    statusP.textContent = anime.status;
+    article.prepend(img);
+    article.append(h3);
+    article.append(p);
+    h3.append(animeRun);
+    h3.append(statusP);
+
+    
+  };
+
+  // const tvShows = (anime) => {
+  //   let animeShows = anime.filter((anime) => anime.type == "TV");
+
+  //   let article = document.createElement("article");
+  //   let h3 = document.createElement("h3");
+  //   let img = document.createElement("img");
+  //   let statusP = document.createElement("p");
+  //   let p = document.createElement("p");
+  //   let animeRun = document.createElement("p");
+
+  //   img.setAttribute("src", animeShows.images.jpg.image_url);
+  //   img.setAttribute("alt", `photo of ${animeShows.title}`);
+  //   h3.textContent = animeShows.title;
+  //   p.textContent = displayAnime.append(article);
+  //   animeRun.textContent = animeShows.aired.string;
+  //   statusP.textContent = animeShows.status;
+  //   article.prepend(img);
+  //   article.append(h3);
+  //   article.append(p);
+  //   h3.append(animeRun);
+  //   h3.append(statusP);
+  // };
 });
-// going to add a ul element so i could add data from the api to the ul so it could look clean and with remove decoration it could display good
