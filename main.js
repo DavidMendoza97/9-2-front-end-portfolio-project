b40ced17920e2374fcceb520e9286bba4a4b7715
Mendoza Holger <mendozadavidh111@gmail.com>
2022-10-02 14:26:29 -0400

    added more code

--- main.js
+++ main.js
@@ -21,42 +21,49 @@ addEventListener("DOMContentLoaded", (e) => {
     let statusP = document.createElement("p");
     let p = document.createElement("p");
     let animeRun = document.createElement("p");
+    let btn = document.createElement("button");
 
     img.setAttribute("src", anime.images.jpg.image_url);
     img.setAttribute("alt", `photo of ${anime.title}`);
     h3.textContent = anime.title;
     p.textContent = displayAnime.append(article);
     animeRun.textContent = anime.aired.string;
-    statusP.textContent = anime.status;
+    // statusP.textContent = anime.status;
+    article.prepend(img);
+    article.append(h3);
+    article.append(p);
+    h3.append(animeRun);
+    // h3.append(statusP);
+    btn.setAttribute("type", "submit");
+    btn.textContent = "more info";
+    article.append(btn)
+    btn.classList.add('info_btn')
+    btn.addEventListener('click',(e) => {
+      e.preventDefault()
+      console.log(e.h3)
+    })
+  };
+
+  const tvShows = (anime) => {
+    let animeShows = anime.filter((anime) => anime.type == "TV");
+
+    let article = document.createElement("article");
+    let h3 = document.createElement("h3");
+    let img = document.createElement("img");
+    let statusP = document.createElement("p");
+    let p = document.createElement("p");
+    let animeRun = document.createElement("p");
+
+    img.setAttribute("src", animeShows.images.jpg.image_url);
+    img.setAttribute("alt", `photo of ${animeShows.title}`);
+    h3.textContent = animeShows.title;
+    p.textContent = displayAnime.append(article);
+    animeRun.textContent = animeShows.aired.string;
+    statusP.textContent = animeShows.status;
     article.prepend(img);
     article.append(h3);
     article.append(p);
     h3.append(animeRun);
     h3.append(statusP);
-
-    
   };
-
-  // const tvShows = (anime) => {
-  //   let animeShows = anime.filter((anime) => anime.type == "TV");
-
-  //   let article = document.createElement("article");
-  //   let h3 = document.createElement("h3");
-  //   let img = document.createElement("img");
-  //   let statusP = document.createElement("p");
-  //   let p = document.createElement("p");
-  //   let animeRun = document.createElement("p");
-
-  //   img.setAttribute("src", animeShows.images.jpg.image_url);
-  //   img.setAttribute("alt", `photo of ${animeShows.title}`);
-  //   h3.textContent = animeShows.title;
-  //   p.textContent = displayAnime.append(article);
-  //   animeRun.textContent = animeShows.aired.string;
-  //   statusP.textContent = animeShows.status;
-  //   article.prepend(img);
-  //   article.append(h3);
-  //   article.append(p);
-  //   h3.append(animeRun);
-  //   h3.append(statusP);
-  // };
 });
